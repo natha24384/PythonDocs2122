f9717351b3cbe432ad28fd56f96e999428d0f5b8
natha24384 <82458776+natha24384@users.noreply.github.com>
2021-11-03 10:43:14 -0400

    Create 01_assignment.py

---
+++ 01_assignment.py
@@ -0,0 +1,38 @@
+# nathan
+# assignment examples
+
+# You can assign "values" to "variables" by using =
+x = 5
+
+# when python reads a variable name it replaces it with the variables stored value
+y = x + 5
+
+# theres 4 diffrent primitive data types
+# intergers; an y whole number positive or negative
+age =  16
+#float: any number with a decimal positive or negative
+grade = 98.6
+#string; a string of human readable characters that are counted as letters 
+name = "chris"
+favoritenumber = "3"
+#boolean; true or false
+# true is any value that isnt false or empty
+isSmart = True
+
+# you can output using print
+print(age)
+print(name)
+print(grade)
+print(isSmart)
+print(favoritenumber)
+
+# you can concatenate values together
+print("my name is " + name)
+# Use functions to covert data types
+print("and my age is " + str(age))
+# assign the converted value to a variable if you want it converted permanently
+age = str(age)
+# you can covert types with int() str() bool() and float()
+print(int(age))
+print(float(age))
+print(bool(age))
